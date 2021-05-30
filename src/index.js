e3b556e9f54b4ca879fe576e8d7a51315b01916f
payalkherajani <payal.kherajani19@gmail.com>
2021-05-30 10:19:29 +0530

    added-css-loader-&-assest-for-images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import App from "./App";
+import Logo from "./logo.svg"
 
 const root = document.getElementById("root");
 root.innerHTML = `
 <h1> learning web tooling today </h1>
+<img src="${Logo}" alt="build icon" />
 ${App({ name: "payalkherajani" })}
 `;
