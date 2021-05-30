90b3428a7e1f88aff35fa269974452e1144bad1a
payalkherajani <payal.kherajani19@gmail.com>
2021-05-30 18:37:48 +0530

    caching-done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import _ from "lodash";
 const App = ({ name }) => {
     return `
-    <h2> ${_.join(["Welcome,", name], " ")} </h2>
+    <h2> ${_.join(["Welcome,", name], " ")} !</h2>
     <p> Build apps with plain JS but with modern tooling  </p>
     `;
 };
